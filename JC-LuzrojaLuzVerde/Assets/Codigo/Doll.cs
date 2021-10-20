3d3ccf90f6d2227675a06381a74995c586e232b9
Erick Olachea <erick.olachea@uabc.edu.mx>
2021-10-19 23:12:23 -0700

    update doll
    actualizacion de audios de instrucciones del juego, implementacion de textura en el terreno

--- JC-LuzrojaLuzVerde/Assets/Codigo/Doll.cs
+++ JC-LuzrojaLuzVerde/Assets/Codigo/Doll.cs
@@ -7,67 +7,77 @@ using System;
 
 public class Doll : MonoBehaviour
 {
-    //public Transform cabeza;
-
-    private bool voltea = false;
-    public TextMeshPro minutos;
-    public TextMeshPro segundos;
+   
+    
     public Transform cabeza;
+    public Transform cuerpo;
     int tiempo;
     private Reloj inicio;
     public AudioSource instrucciones1;
     public AudioSource instrucciones2;
     public AudioSource[] luz;
-    private bool start = false;
+    public AudioSource[] doll;
+    private bool start = true;
     // Start is called before the first frame update
     void Start()
     {
         
         inicio = FindObjectOfType<Reloj>();
-        
+        instrucciones1.Play(1);
+        rotation_Doll(cuerpo, 180);
+        rotation_Doll(cabeza, 180);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (instrucciones1.isPlaying)
-        {
-            instrucciones2.PlayDelayed(1);
-        }
-        if (!instrucciones2.isPlaying && !instrucciones1.isPlaying)
-        {
-            inicio.start = true;
-           
-        }
-        //tiempo = Convert.ToInt32(segundos.text);
-        if (inicio.Seg >= 30) 
-        {
-            cabeza_Doll(180);
-        }
-        if (inicio.Seg <= 30)
-        {
-            cabeza_Doll(0);
-        }
+        inicio_Audio();
+
+        //if (inicio.Seg >= 30) 
+        //{
+        //   rotation_Doll(cabeza,180);
+        //}
+        //if (inicio.Seg <= 30)
+        //{
+        //    rotation_Doll(cabeza,0);
+        //}
     }
+
     public void inicio_Audio() 
     {
-        if (!start)
-        {
-            if (!instrucciones1.isPlaying)
+        //booleano para iniciar solo una ves las instrucciones
+        if (start) {
+
+            if (instrucciones1.isPlaying)
             {
-               
-                instrucciones2.Play(1);
+                instrucciones2.PlayDelayed(1);
+                
+                doll[1].PlayDelayed(1);
+                
             }
-            else if (!instrucciones2.isPlaying)
+
+            if (!instrucciones2.isPlaying && !instrucciones1.isPlaying)
             {
                 inicio.start = true;
-                start = true;
+                start = false;
+
+            }
+            if (instrucciones2.isPlaying)
+            {
+                rotation_Doll(cuerpo, 180);
+                rotation_Doll(cabeza, 180);
+            }
+            else 
+            {
+                rotation_Doll(cuerpo, 0);
+                rotation_Doll(cabeza, 0);
             }
         }
     }
-    public void cabeza_Doll(int grados) 
+    public void rotation_Doll(Transform obj,int grados) 
     {
        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, grados, transform.rotation.eulerAngles.z)), Time.deltaTime * 3);
-       cabeza.rotation = Quaternion.Slerp(cabeza.rotation, Quaternion.Euler(new Vector3(cabeza.rotation.eulerAngles.x, grados, cabeza.rotation.eulerAngles.z)), Time.deltaTime * 3);
+       obj.rotation = Quaternion.Slerp(obj.rotation, Quaternion.Euler(new Vector3(obj.rotation.eulerAngles.x, grados, obj.rotation.eulerAngles.z)), Time.deltaTime * 3);
     }
 }
