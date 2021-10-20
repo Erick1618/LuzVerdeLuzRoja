bbefd93a4d21e0a6cfb77041403ce48bf995036e
Cachigod <santiago.erick@uabc.edu.mx>
2021-10-19 23:09:08 -0700

    Primeros Movimientos Jugador
    Se creó el código para los movimientos básicos del jugador y se agregaron al objeto del personaje.

---
+++ JC-LuzrojaLuzVerde/Assets/Codigo/LogicaPersonaje1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje1 : MonoBehaviour
+{
+    public float velocidadMov = 5.0f;
+    public float velocidadRot = 200.0f;
+    private Animator anim;
+    public float x, y; // Saber si nuestro personaje se está moviendo
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * velocidadRot, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMov);
+    }
+}
