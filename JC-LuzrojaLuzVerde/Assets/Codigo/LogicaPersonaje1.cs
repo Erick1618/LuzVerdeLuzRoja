35573017e75cd9f93124c2e3d8b06543e36d39f6
Cachigod <santiago.erick@uabc.edu.mx>
2021-10-21 01:10:38 -0700

    Animaciones Salto y Correr
    Se implementaron las animaciones de correr y saltar del jugador. y se cambió el movimiento base de corriendo a caminando, modificando las respectivas velocidades del jugador dependiendo si está caminando o corriendo.

--- JC-LuzrojaLuzVerde/Assets/Codigo/LogicaPersonaje1.cs
+++ JC-LuzrojaLuzVerde/Assets/Codigo/LogicaPersonaje1.cs
@@ -4,26 +4,91 @@ using UnityEngine;
 
 public class LogicaPersonaje1 : MonoBehaviour
 {
-    public float velocidadMov = 5.0f;
+    public float velocidadInicial = 5.0f;
+    public float velocidadMov;
+    public int velocidadCorrer = 10;
     public float velocidadRot = 200.0f;
+
     private Animator anim;
     public float x, y; // Saber si nuestro personaje se está moviendo
+
+    public Rigidbody rb;
+    public float fuerzaDeSalto = 12f;
+    public bool puedoSaltar;
+
     // Start is called before the first frame update
     void Start()
     {
+        puedoSaltar = false;
+        velocidadMov = velocidadInicial;
         anim = GetComponent<Animator>();
     }
 
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * velocidadRot, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMov);
+    }
     // Update is called once per frame
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        transform.Rotate(0, x * Time.deltaTime * velocidadRot, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMov);
-
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
+
+        if(puedoSaltar)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("brinque", true);
+                rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+            }
+            anim.SetBool("tocoSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if(puedoSaltar) // Estoy en el piso
+            {
+                velocidadMov = velocidadCorrer;
+                if(y > 0)
+                {
+                    // Entonces estamos avanzando
+                    anim.SetBool("corriendo", true);
+                }
+                else
+                {
+                    anim.SetBool("corriendo", false);
+                }
+            }
+            else
+            {
+                anim.SetBool("corriendo", false);
+                if (puedoSaltar)
+                {
+                    velocidadMov = velocidadInicial;
+                }
+            }
+        }
+        else
+        {
+            anim.SetBool("corriendo", false);
+            if(puedoSaltar)
+            {
+                velocidadMov = velocidadInicial;
+            }
+        }
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("tocoSuelo", false);
+        anim.SetBool("brinque", false);
     }
 }
