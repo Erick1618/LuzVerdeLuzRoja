bbb80a57ef4c526b6b27198ed9cf97272e790fdc
Cachigod <santiago.erick@uabc.edu.mx>
2021-10-21 18:34:01 -0700

    Unión de escenario con animaciones
    Se agregaron las animaciones del jugador completas junto al escenario.

--- JC-LuzrojaLuzVerde/Assets/Codigo/LogicaPersonaje1.cs
+++ JC-LuzrojaLuzVerde/Assets/Codigo/LogicaPersonaje1.cs
@@ -1,54 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LogicaPersonaje1 : MonoBehaviour
 {
-    public float velocidadMov = 5.0f;
+    public float velocidadInicial = 5.0f;
+    public float velocidadMov;
+    public int velocidadCorrer = 10;
     public float velocidadRot = 200.0f;
+
     private Animator anim;
-    private float y; // Saber si nuestro personaje se está moviendo
     private float x;
-    
-    private Doll muneca;
-    private Vector3 posicioninicial;
+    private float y; // Saber si nuestro personaje se está moviendo
+
+    public Rigidbody rb;
+    public float fuerzaDeSalto = 12f;
+    public bool puedoSaltar;
     public bool estadendro = false;
-    private Quaternion xsd;
-    public Transform persona;
 
     public float Y { get => y; set => y = value; }
     public float X { get => x; set => x = value; }
 
-
     // Start is called before the first frame update
     void Start()
     {
+        puedoSaltar = false;
+        velocidadMov = velocidadInicial;
         anim = GetComponent<Animator>();
-        muneca = FindObjectOfType<Doll>();
-       
     }
 
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * velocidadRot, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMov);
+    }
     // Update is called once per frame
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-        
-        transform.Rotate(0, x * Time.deltaTime * velocidadRot, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMov);
 
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
 
-       
+        if(puedoSaltar)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("brinque", true);
+                rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+            }
+            anim.SetBool("tocoSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+            anim.SetBool("tocoSuelo", false);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if(puedoSaltar) // Estoy en el piso
+            {
+                velocidadMov = velocidadCorrer;
+                if(y > 0)
+                {
+                    // Entonces estamos avanzando
+                    anim.SetBool("corriendo", true);
+                }
+                else
+                {
+                    anim.SetBool("corriendo", false);
+                }
+            }
+            else
+            {
+                anim.SetBool("corriendo", false);
+                if (puedoSaltar)
+                {
+                    velocidadMov = velocidadInicial;
+                }
+            }
+        }
+        else
+        {
+            anim.SetBool("corriendo", false);
+            if(puedoSaltar)
+            {
+                velocidadMov = velocidadInicial;
+            }
+        }
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("tocoSuelo", false);
+        anim.SetBool("brinque", false);
     }
     private void OnTriggerEnter(Collider obj)
     {
         if (obj.tag == "AM")
         {
             estadendro = true;
-           
 
         }
 
@@ -57,12 +110,11 @@ public class LogicaPersonaje1 : MonoBehaviour
     {
         if (obj.tag == "AM")
         {
-
             estadendro = false;
-            
         }
 
     }
-   
-  
+
+
+
 }
---
+++ JC-LuzrojaLuzVerde/Assets/Codigo/LogicaPies.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public LogicaPersonaje1 logicaP1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        logicaP1.puedoSaltar = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        logicaP1.puedoSaltar = false;
+    }
+}
